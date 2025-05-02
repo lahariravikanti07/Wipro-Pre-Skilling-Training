fd9d9a754bdff3064a33a1cd31af7034a6b77a46
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-02 12:13:05 +0530

    Find Pairs With Sum[Brute Force Approach]

---
+++ May2/Find Pairs With Sum.cs
@@ -0,0 +1,27 @@
+using System;
+//BRUTE FORCE APPROACH
+
+class Program
+{
+    static void FindPairsWithSum(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[i] + arr[j] == target)
+                {
+                    Console.WriteLine($"{arr[i]} + {arr[j]} = {target}");
+                }
+            }
+        }
+    }
+    
+    static void Main()
+    {
+        int[] numbers = { 2, 4, 3, 5, 7 };
+        int targetSum = 9;
+        
+        FindPairsWithSum(numbers, targetSum);
+    }
+}
