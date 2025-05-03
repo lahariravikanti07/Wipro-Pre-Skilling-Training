742692ee8f83340c8b00ab97fad929b0534f0d69
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-03 13:09:38 +0530

    Employees Table Value Insertion

---
+++ May3/CRUD Operations/Value Insertion .sql
@@ -0,0 +1,7 @@
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department, Salary, HireDate)
+VALUES
+(1, 'John', 'Doe', 'HR', 5000.00, '2022-03-15'),
+(2, 'Jane', 'Smith', 'IT', 7500.00, '2021-01-11'),
+(3, 'Robert', 'Brown', 'Finance', 6200.05, '2023-08-21'),
+(4, 'Emily', 'Clark', 'Marketing', 4500.00, '2024-09-24'),
+(5, 'Micheal', 'Davis', 'Operations', 6900.00, '2025-02-25')
