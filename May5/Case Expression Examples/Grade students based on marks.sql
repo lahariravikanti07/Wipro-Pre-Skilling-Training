702605c6f817ef5ff396de04135a44ba08c69271
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-05 12:35:02 +0530

    Grade students based on marks

---
+++ May5/Case Expression Examples/Grade students based on marks.sql
@@ -0,0 +1,8 @@
+SELECT FirstName, Marks,
+    CASE
+       WHEN Marks >= 90 THEN 'A'
+       WHEN Marks >= 75 THEN 'B'
+       WHEN Marks >= 60 THEN 'C'
+       ELSE 'D'
+    END AS Grade
+FROM Students;
