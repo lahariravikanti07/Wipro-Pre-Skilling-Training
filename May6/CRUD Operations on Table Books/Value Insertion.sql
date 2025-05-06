e45fdf3578d0a680dff803bbf26502af1fe2287f
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-06 09:19:47 +0530

    Value Insertion

---
+++ May6/CRUD Operations on Table Books/Value Insertion.sql
@@ -0,0 +1,6 @@
+INSERT INTO Books (BookID, Title, Author, Genre, Price, PublishedYear)
+VALUES
+(1, 'Atomic Habits', 'James Clear', 'Self-Help', 499.00, 2018),
+(2, 'The Alchemist', 'Paulo Coelho', 'Fiction', 299.00, 1993),
+(3, 'Clean Code', 'Robert C. Martin', 'Programming', 799.00, 2008),
+(4, 'Sapiens', 'Yuval Noah Harari', 'History', 599.00, 2015);
