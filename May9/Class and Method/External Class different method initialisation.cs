eb867099a0b625c391d2877e669b24cac2584efe
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-09 10:09:36 +0530

    Different method initialisation for external class

---
+++ May9/Class and Method/External Class different method initialisation.cs
@@ -0,0 +1,18 @@
+using System;
+// Using ConsoleApp1
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            Student student1 = new Student { RollNumber = 101, Name = "Neha", Marks = 90 };
+            Student student2 = new Student { RollNumber = 102, Name = "Rahul", Marks = 75 };
+
+            student1.PrintDetails();
+            student2.PrintDetails();
+        }
+    }
+}
