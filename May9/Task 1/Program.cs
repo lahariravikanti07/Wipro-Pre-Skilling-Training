e99af6634f28eac5097ae6c6c6647e4847b6f909
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-09 10:45:23 +0530

    This file should only contain the Employee class

---
+++ May9/Task 1/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace EmployeeManagement
+{
+    // Class Definition
+    public class Employee
+    {
+        // Fields
+        public int EmployeeID;
+        public string Name;
+        public string Department;
+        public double Salary;
+
+        // Constructor
+        public Employee(int id, string name, string dept, double salary)
+        {
+            EmployeeID = id;
+            Name = name;
+            Department = dept;
+            Salary = salary;
+        }
+
+        // Prints employee details
+        public void PrintDetails()
+        {
+            Console.WriteLine($"ID: {EmployeeID}, Name {Name}, Department: {Department}, Salary: {Salary}");  
+        }
+
+        // Updates salary
+        public void UpdateSalary(double newSalary)
+        {
+            Salary = newSalary;
+            Console.WriteLine("Salary updated.");
+        }
+
+        // Calculates annual salary
+        public double AnnualSalary()
+        {
+            return Salary * 12;
+        }
+    }
+}
