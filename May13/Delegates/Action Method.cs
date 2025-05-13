88b0dfc487435949c1ce761da663444da33a6861
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-05-13 12:54:31 +0530

    Action Method

---
+++ May13/Delegates/Action Method.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Linq_Practice.Practice1
+{
+    class Action
+    {
+        public static void Main()
+        {
+            Action<int> printActionDel = ConsolePrint;
+            Action<int> printToPrinter = PrinterPrint;
+
+            printActionDel(10);
+            printToPrinter(45);
+            Console.Read();
+        }
+
+        public static void ConsolePrint(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        public static void PrinterPrint(int i)
+        {
+            Console.WriteLine("This is being printed to the printer");
+        }
+    }
+}
+    
