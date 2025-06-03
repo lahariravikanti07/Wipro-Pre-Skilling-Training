98da07f82f5e71832cf12f8ca814714b43d825d9
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-06-03 11:05:13 +0530

    27381

---
+++ June3/Web API/TaskOfJune3/TaskOfJune3/Data/TaskOfJune3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskOfJune3.Models;
+
+namespace TaskOfJune3.Data
+{
+    public class TaskOfJune3Context : DbContext
+    {
+        public TaskOfJune3Context (DbContextOptions<TaskOfJune3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskOfJune3.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ June3/Web API/TaskOfJune3/TaskOfJune3/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskOfJune3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Brand { get; set; }
+
+    }
+}
