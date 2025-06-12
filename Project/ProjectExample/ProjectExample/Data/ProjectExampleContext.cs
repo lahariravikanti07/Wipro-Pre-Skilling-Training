8961680a6f750db0168457360fa466a3b2ed12c7
Lahari Ravikanti <rlkrishna2002@gmail.com>
2025-06-12 10:11:41 +0530

    27381

---
+++ Project/ProjectExample/ProjectExample/Data/ProjectExampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectExample.Models;
+
+namespace ProjectExample.Data
+{
+    public class ProjectExampleContext : DbContext
+    {
+        public ProjectExampleContext (DbContextOptions<ProjectExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectExample.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Project/ProjectExample/ProjectExample/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectExample.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int PublishedYear { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
